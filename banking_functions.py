2ef966d27b0db62e79ac0989e69015dae0d97bec
sarahhanafy <70714058+sarahhanafy@users.noreply.github.com>
2024-04-29 11:43:38 -0700

    changed

--- banking_functions.py
+++ banking_functions.py
@@ -1,21 +1,73 @@
 import sqlite3
 
-# Connect to the SQLite database
-conn = sqlite3.connect('banking_system.db')
-cursor = conn.cursor()
+# Function to create a new account
+def create_account(account_number, pin, account_type):
+    conn = sqlite3.connect('banking_system.db')
+    cursor = conn.cursor()
 
-def create_account(username, pin, account_type):
-    # Implement account creation functionality
-    pass
+    cursor.execute('''
+    INSERT INTO users (account_number, pin, account_type)
+    VALUES (?, ?, ?)
+    ''', (account_number, pin, account_type))
 
-def check_balance(user_id):
-    # Implement balance checking functionality
-    pass
+    # Get the user_id of the newly created user
+    user_id = cursor.lastrowid
 
-def deposit(user_id, amount):
-    # Implement deposit functionality
-    pass
+    cursor.execute('''
+    INSERT INTO accounts (user_id, balance)
+    VALUES (?, ?)
+    ''', (user_id, 0))
 
-def withdraw(user_id, amount):
-    # Implement withdrawal functionality
-    pass
+    conn.commit()
+    conn.close()
+
+    return user_id
+
+# Function to check account balance
+def check_balance(account_number):
+    conn = sqlite3.connect('banking_system.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    SELECT balance FROM accounts
+    INNER JOIN users ON accounts.user_id = users.user_id
+    WHERE users.account_number = ?
+    ''', (account_number,))
+
+    balance = cursor.fetchone()[0]
+
+    conn.close()
+
+    return balance
+
+# Function to make a deposit
+def deposit(account_number, amount):
+    conn = sqlite3.connect('banking_system.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    UPDATE accounts
+    SET balance = balance + ?
+    WHERE user_id = (
+        SELECT user_id FROM users WHERE account_number = ?
+    )
+    ''', (amount, account_number))
+
+    conn.commit()
+    conn.close()
+
+# Function to make a withdrawal
+def withdraw(account_number, amount):
+    conn = sqlite3.connect('banking_system.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    UPDATE accounts
+    SET balance = balance - ?
+    WHERE user_id = (
+        SELECT user_id FROM users WHERE account_number = ?
+    )
+    ''', (amount, account_number))
+
+    conn.commit()
+    conn.close()
